2702b88301cacc8dc4ef854028b52a0c255aa100
AminesCodes <aminebensalem@pursuit.org>
2020-03-17 21:02:40 -0400

    Polishing the display

--- src/components/HomeCardBack.jsx
+++ src/components/HomeCardBack.jsx
@@ -8,7 +8,8 @@ export default (props) => {
         >
             <p>For as long as I can remember, I have been always curious about how things work and having a passion for Tech.</p>
             <p>This curiosity and passion led me to a Bachelor of science in automation engineering technology.</p>  
-            <p>Seeking a career shift after moving to New York City, and new challenges, 
+            <p>Seeking new challenges and a career shift after moving to New York City, 
+                while keeping the problem solving aspect that I enjoy, 
                 I enrolled into an intensive full-stack web development fellowship program at Pursuit 
                 where I gained exposure to different coding languages through project based learning.
             </p>
