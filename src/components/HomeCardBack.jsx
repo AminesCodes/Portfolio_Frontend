fb995788f959bc7f2e398256abd962976f2c5121
AminesCodes <aminebensalem@pursuit.org>
2020-03-20 03:08:50 -0400

    Polishing the display

--- src/components/HomeCardBack.jsx
+++ src/components/HomeCardBack.jsx
@@ -11,7 +11,7 @@ export default (props) => {
             <p>Seeking new challenges and a career shift after moving to New York City, 
                 while keeping the problem solving aspect that I enjoy, 
                 I enrolled into an intensive and highly selective full-stack web development fellowship program at Pursuit 
-                where I gained the necessary technical and professional skills through project based learning and team work
+                where I gained the necessary technical and professional skills, through project based learning and team work,
                 to be a successful software engineer.
             </p>
         </div>
