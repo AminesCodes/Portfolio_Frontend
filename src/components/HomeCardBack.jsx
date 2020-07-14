37f3c7d6c7ccff74eeb52c99cce93ecf162e0d03
AminesCodes <aminebensalem@pursuit.org>
2020-07-14 17:41:03 -0400

    Fix a typo

--- src/components/HomeCardBack.jsx
+++ src/components/HomeCardBack.jsx
@@ -4,7 +4,7 @@ export default (props) => {
     return (
         <div className='h-100 text-justify p-3' >
             <p>For as long as I can remember, I have always been curious about how things work passionate about technology.</p>
-            <p>This curiosity and passion led me to a Bachelor of science in automation engineering technology.</p>  
+            <p>This curiosity and passion led me to a Bachelor of Science in automation engineering technology.</p>  
             <p>Seeking new challenges after moving to New York City,
                 I enrolled into an intensive and highly selective full-stack web development fellowship program at Pursuit 
                 where I gained valuable technical and professional skills, through project based learning and team work.
